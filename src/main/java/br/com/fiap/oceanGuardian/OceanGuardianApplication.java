722f9b61c9a0457630de7979c503e95678821e75
olivanaa <oliv.anaclara@gmail.com>
2024-06-06 21:01:34 -0300

    mudança em "/"

--- src/main/java/br/com/fiap/oceanGuardian/OceanGuardianApplication.java
+++ src/main/java/br/com/fiap/oceanGuardian/OceanGuardianApplication.java
@@ -3,12 +3,16 @@ package br.com.fiap.oceanGuardian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@Controller
 @EnableCaching
 @OpenAPIDefinition(
 	info = @Info(
@@ -22,6 +26,12 @@ public class OceanGuardianApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OceanGuardianApplication.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody 
+	public String home(){
+		return "Ocean Guardian";
 	}
 
 }
